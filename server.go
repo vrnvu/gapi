f6442e067e86e32bd3076b4300ef5680eb521bf7
Arnau Diaz <diazarnau0@gmail.com>
2020-12-31 11:57:17 +0100

    feature: server fetches data from json file

--- server.go
+++ server.go
@@ -5,10 +5,10 @@ package main
 // We could use psql, mdb or json files
 // I like json files for starters
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
-	"encoding/json"
 )
 
 // NOTE I don't like this api design
@@ -25,11 +25,13 @@ func NewServer() Server {
 	a := &api{}
 	r := mux.NewRouter()
 
+	r.HandleFunc("/ini", a.ini).Methods(http.MethodGet)
+	r.HandleFunc("/del", a.del).Methods(http.MethodGet)
+
 	r.HandleFunc("/employees", a.fetchEmployees).Methods(http.MethodGet)
 
 	// We capture name and define a valid regex condition
 	r.HandleFunc("/employee/{name:[a-z]+}", a.fetchEmployee).Methods(http.MethodGet)
-
 
 	r.HandleFunc("/employee", a.createEmployee).Methods(http.MethodPost)
 
@@ -42,30 +44,50 @@ func (a *api) Router() http.Handler {
 	return a.router
 }
 
-func (a *api) fetchEmployees(w http.ResponseWriter, r *http.Request) {
+// TODO mutex or some sort of sync
+func (a *api) ini(w http.ResponseWriter, r *http.Request) {
 	employees := []Employee{
 		Employee{"a", 100, 10},
 		Employee{"b", 200, 20},
 		Employee{"c", 300, 30},
+		Employee{"d", 400, 40},
+		Employee{"e", 500, 50},
 	}
+	WriteEmployees(employees)
+}
+
+// TODO mutex or some sort of sync
+func (a *api) del(w http.ResponseWriter, r *http.Request) {
+	WriteEmployees(make([]Employee, 0))
+}
+
+// TODO mutex or some sort of sync
+func (a *api) fetchEmployees(w http.ResponseWriter, r *http.Request) {
+	employees := ReadEmployees()
 	fmt.Println("debug:", employees)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(employees)
 }
 
+// TODO mutex or some sort of sync
 func (a *api) fetchEmployee(w http.ResponseWriter, r *http.Request) {
 	// Returns values captured in the request URL
 	// vars is a dictionary whose key-value pairs are variables
 	vars := mux.Vars(r)
 	fmt.Println("debug:", vars)
 
-	employee := Employee{"a", 100, 10}
+	employees := ReadEmployees()
+
+	employee, err := FindEmployee(vars["name"], employees)
+	if err != nil {
+		fmt.Println("debug:", err)
+	}
 	fmt.Println("debug:", employee)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(employee)
 }
 
-
+// TODO mutex or some sort of sync
 func (a *api) createEmployee(w http.ResponseWriter, r *http.Request) {
 	// curl --header "Content-Type: application/json" --request POST --data '{"name":"xyz","salary":1500, "sales":30}' http://localhost:8080/employee
 	// We attempt to unmarshall our r.Body into an Employee
@@ -77,5 +99,10 @@ func (a *api) createEmployee(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Println("debug:", employee)
+
+	employees := ReadEmployees()
+	employees = append(employees, employee)
+	WriteEmployees(employees)
+
 	w.WriteHeader(http.StatusCreated)
 }
